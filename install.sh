e155e1f2d951eefbe98b0d0c3a40d2ab7f300a95
Fabio Moreno <fabiomoreno@outlook.com>
2020-03-10 17:10:18 -0500

    add zsh installer

--- install.sh
+++ install.sh
@@ -13,9 +13,13 @@ chmod +x installers/*
 
 # install zsh
 apt update
-apt install zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s $(which zsh)
+apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+
+#install node version manager nvm
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh
+sh -c bash install_nvm.sh
 
 # golang
 add-apt-repository -y ppa:longsleep/golang-backports

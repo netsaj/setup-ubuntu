9ef704b8caf7fbac0bfee327e6642048ee1d6a35
Fabio Moreno <fabiomoreno@outlook.com>
2020-03-14 05:28:55 -0500

    fix: solved error in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PASSWORD=""
-if [ $1 = "" ]; then
+if [ -z "$1" ]
+then
     PASSWORD=$(whiptail --passwordbox "please enter your sudo password" 8 78 --title "Password for sudo install" 3>&1 1>&2 2>&3)
 else
     PASSWORD=$1
@@ -15,10 +16,11 @@ CHOISE=$(
     "04)" "Install tools" \
     "05)" "Customize and install themes" 3>&2 2>&1 1>&3	
 )
-echo $CHOISE
-if [ $CHOISE = "01)" ]; then
+echo ${CHOISE}
+
+if  [ "${CHOISE}" = "01)" ]
+then
     ./install_dependencies.sh $PASSWORD
-fi
-if [ $CHOISE = "02)" ]; then
+elif [ "${CHOISE}" = "02)" ]; then
     ./install_ides.sh $PASSWORD
 fi

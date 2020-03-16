dea8eb7714410fc35d0d07097c0396d8ac29d89b
Fabio Moreno <fabiomoreno@outlook.com>
2020-03-16 15:15:03 -0500

    refactor: install vscode using deb file

--- scripts/ubuntu/ides/vscode.sh
+++ scripts/ubuntu/ides/vscode.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 PASSWORD=$1
-echo $PASSWORD | sudo --stdin apt install snapd
-SNAPD_IN_PATH=$(cat $HOME/.bashrc | grep --count "export PATH=/snap/bin:")
-if [ $SNAPD_IN_PATH -eq "0" ]; then
-    echo "export PATH=/snap/bin:\$PATH" >> ~/.bashrc
-fi
-export PATH=/snap/bin:PATH
-echo $PASSWORD | sudo --stdin systemctl start snapd.service
-echo $PASSWORD | sudo --stdin snap install --classic code
+curl -o code.deb -L http://go.microsoft.com/fwlink/?LinkID=760868 
+echo $PASSWORD | sudo --stdin sudo apt install --yes ./code.deb
+rm -rf ./code.deb
